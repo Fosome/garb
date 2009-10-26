3894f9c16b9b005b0fd77a55e4c8d2ea25067c22
tpitale <tpitale@gmail.com>
2009-10-26 14:39:55 -0400

    fixing namespace issue

--- lib/garb/report_response.rb
+++ lib/garb/report_response.rb
@@ -34,7 +34,7 @@ module Garb
       include HappyMapper
 
       tag 'metric'
-      namespace 'http://schemas.google.com/dataexport/2009'
+      namespace 'http://schemas.google.com/analytics/2009'
 
       attribute :name, String
       attribute :value, String
@@ -44,7 +44,7 @@ module Garb
       include HappyMapper
 
       tag 'dimension'
-      namespace 'http://schemas.google.com/dataexport/2009'
+      namespace 'http://schemas.google.com/analytics/2009'
 
       attribute :name, String
       attribute :value, String
